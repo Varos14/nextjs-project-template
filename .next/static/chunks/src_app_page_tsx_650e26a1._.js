291ba07b30bf17a71aa4a60bed5380e0aff46ba0
Gelard Mutwezi <geraldvaros@gmail.com>
2025-07-21 19:25:04 +0300

    Add .next/static/chunks/src_app_page_tsx_650e26a1._.js 1753115103422

---
+++ .next/static/chunks/src_app_page_tsx_650e26a1._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/src_c846c1dc._.js",
+  "static/chunks/node_modules_2f291142._.js"
+],
+    source: "dynamic"
+});
